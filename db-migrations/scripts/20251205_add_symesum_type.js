bd3c4a285da76d722a83ce1693af8691491e08d1
lakshmisairajcode-png <lakshmisairaj.code@gmail.com>
2025-12-05 18:58:01 +0530

    Update 20251205_add_symesum_type.js

--- db-migrations/scripts/20251205_add_symesum_type.js
+++ db-migrations/scripts/20251205_add_symesum_type.js
@@ -1,1 +1,29 @@
+const changelog = "migration_changelog";
+const id = "20251205_add_symesum_type_v1";
 
+if (db.getCollection(changelog).findOne({ id })) {
+  print("Already applied:", id);
+  quit();
+}
+
+db.getCollection("document_types").updateOne(
+  { code: "CPT" },
+  { 
+    $set: { 
+      code: "CPT", 
+      label: "SYMESUM", 
+      active: true, 
+      updatedAt: new Date() 
+    } 
+  },
+  { upsert: true }
+);
+
+db.getCollection(changelog).insertOne({
+  id,
+  file: "20251205_add_symesum_type.js",
+  appliedAt: new Date(),
+  by: "jenkins"
+});
+
+print("Migration applied:", id);
