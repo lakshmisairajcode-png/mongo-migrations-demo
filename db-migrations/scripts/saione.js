a7e324458944f70472d313e63dc09703fee8bac1
lakshmisairajcode-png <lakshmisairaj.code@gmail.com>
2025-12-05 20:10:11 +0530

    Create saione.js

---
+++ db-migrations/scripts/saione.js
@@ -0,0 +1,30 @@
+const changelog = "migration_changelog";
+const id = "20251206_update_cpt_label_to_sai_v1";
+
+if (db.getCollection(changelog).findOne({ id })) {
+  print("Already applied:", id);
+  quit();
+}
+
+db.getCollection("document_types").updateOne(
+  { code: "CPT" },
+  {
+    $set: {
+      code: "CPT",
+      label: "sai",
+      active: true,
+      name: "sai",
+      updatedAt: new Date()
+    }
+  },
+  { upsert: true }
+);
+
+db.getCollection(changelog).insertOne({
+  id,
+  file: "20251206_update_cpt_label_to_sai.js",
+  appliedAt: new Date(),
+  by: "jenkins"
+});
+
+print("Migration applied:", id);
